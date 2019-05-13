6b979df5a1cec932dbb87b476572d93fb4a53d5d
yohan2127 <zedamek@yahoo.com>
2019-05-12 23:44:46 -0400

    add my app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,51 +1,51 @@
 var friends = [
 	
 	{
-		name: "Benjamin Browder",
+		name: "Zewdu Yohannis",
 		photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		scores: [
-			"5",
-			"1",
-			"4",
-			"3",
 			"1",
 			"5",
-			"1",
+			"2",
 			"4",
 			"3",
+			"3",
+			"4",
+			"2",
+			"5",
 			"1"
 		]
 	},
 	{
-		name: "Vladamir VanDamme",
+		name: "Nathan Mekoya",
 		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		scores: [
-			"3",
-			"1",
 			"5",
 			"4",
+			"3",
+			"2",
 			"1",
-			"5",
 			"1",
-			"1",
-			"5",
-			"3"
+			"2",
+			"3",
+			"4",
+			"5"
 		]
 	},
 	{
-		name: "Brian Eastman",
+		name: "Nahu Kebe",
 		photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		score: [
-			"3",
-			"1",
-			"4",
-			"1",
-			"4",
+			"2",
 			"3",
 			"5",
+			"2",
+			"4",
+			"2",
+			"3",
 			"1",
-			"5",
-			"4"		
+			"1",
+			"3"		
 		]
 	}
 ];
